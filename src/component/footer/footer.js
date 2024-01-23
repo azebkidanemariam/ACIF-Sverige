1b60fff09ceed61bf07aced2073a517f012decb3
Azeb Kidanemariam <azeb.kidanemariam@knowit.se>
2024-01-23 12:54:53 +0100

    make footer more responsiv and cahnge styling

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -5,54 +5,50 @@ import { FaFacebook, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
 
 function FooterComponent() {
   return (
-    <footer
-      className="text-start  container-fluid p-0 "
-      style={{ color: "#fff" }}
-    >
-      <div className="shadow-lg p-3  mt-4  rounded ">
+    <footer className="text-start" style={{ color: "#fff" }}>
+      <div className="shadow-lg mt-4 rounded p-4 ">
         <div className="row d-flex justify-content-between ">
           <ul className="col-md-4 col-12 ">
             <h4>Contact Us</h4>
             <li>
-              <strong>Email - </strong> apostolic_swe@yahoo.com
+              <strong>Email Address - </strong> apostolic_swe@yahoo.com
             </li>
             <li>
               <strong>Telephone - </strong> +46 73 700 5226
             </li>
             <li>
-              <strong>Church Address - </strong> BorgenVägen 16 Danderyd
+              <strong>Church Address - </strong> Borgenvägen 16 Danderyd
             </li>
             <li>
-              <strong>Postal Address - </strong> FrihetsVägen 54 17733 Järfalla
+              <strong>Postal Address - </strong> Frihetsvägen 54 17733 Järfalla
             </li>
           </ul>
 
-          <ul className="col-md-2 col-6">
+          <ul className="col-md-3 col-10">
             <h4>Services</h4>
-            <li>Sunday Service</li>
-            <li>Bible Studying</li>
-            <li>Sunday School</li>
-            <li>Prayer Service</li>
+            <li>Sunday Services - 13-00 - 16.00</li>
+            <li>Bible Studying - Tuesday and Friday</li>
+            <li>Sunday School - Sunday 14:30</li>
+            <li>Prayer Service - Tuesday and Friday</li>
           </ul>
         </div>
-
-        <div className="social-media nav-style  ">
-          <h6>You can find us at</h6>
-          <ul className="list-unstyled list-inline d-flex justify-content-center ">
-            <li className="list-inline-item">
-              <FaFacebook />
-            </li>
-            <li className="list-inline-item">
-              <FaLinkedin />
-            </li>
-            <li className="list-inline-item">
-              <FaTwitter />
-            </li>
-            <li className="list-inline-item">
-              <FaYoutube />
-            </li>
-          </ul>
-        </div>
+      </div>
+      <div className="social-media nav-style  ">
+        <h6>You can find us at</h6>
+        <ul className="list-unstyled list-inline d-flex justify-content-center ">
+          <li className="list-inline-item">
+            <FaFacebook />
+          </li>
+          <li className="list-inline-item">
+            <FaLinkedin />
+          </li>
+          <li className="list-inline-item">
+            <FaTwitter />
+          </li>
+          <li className="list-inline-item">
+            <FaYoutube />
+          </li>
+        </ul>
       </div>
     </footer>
   );
