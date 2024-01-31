0f6e432ecfc89707f41f014ef0592f739d681b28
Azeb Kidanemariam <azeb.kidanemariam@knowit.se>
2024-01-31 15:11:39 +0100

    change linear background for question and answer page

--- src/component/questionAnswer/accordion.js
+++ src/component/questionAnswer/accordion.js
@@ -5,7 +5,7 @@ import { FaChevronUp, FaChevronCircleDown } from "react-icons/fa";
 export const Accordion = ({ title, content, verse = "" }) => {
   const [isActive, setIsActive] = useState(false);
   return (
-    <div className="pt-6">
+    <div className="pt-6 mt-4">
       <div className="panel-group" onClick={() => setIsActive(!isActive)}>
         <div className="panel-heading">
           <div className="panel-title d-flex justify-content-between align-items-center">
